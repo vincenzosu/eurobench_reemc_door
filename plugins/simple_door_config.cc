a7e6d2cd99d8a5a699d8b3bb82baa0fa83825cf4
Vincenzo Suriani <vincsur@gmail.com>
2021-01-08 18:23:04 +0100

    ROS transport

--- plugins/simple_door_config.cc
+++ plugins/simple_door_config.cc
@@ -6,6 +6,22 @@
 #include <stdlib.h>
 #include "LUT.h"
 
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "madrob/gui/benchmark_params.h"
+
+
+/// \brief A node use for ROS transport
+private: std::unique_ptr<ros::NodeHandle> rosNode;
+/// \brief A ROS subscriber
+private: ros::Subscriber rosSub;
+/// \brief A ROS callbackqueue that helps process messages
+private: ros::CallbackQueue rosQueue;
+/// \brief A thread the keeps running the rosQueue
+private: std::thread rosQueueThread;
+
 
 namespace gazebo {
   
