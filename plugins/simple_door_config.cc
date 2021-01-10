c017276d2aff0d7623a961ee954274733b417eb8
Vincenzo Suriani <vincsur@gmail.com>
2021-01-10 19:55:15 +0100

    param

--- plugins/simple_door_config.cc
+++ plugins/simple_door_config.cc
@@ -118,7 +118,7 @@ namespace gazebo {
           // ****************** dal tutorial
         //ros::NodeHandle n;
 //        ros::ServiceClient client = n.serviceClient<eurobench_bms_msgs_and_srvs::MadrobBenchmarkParams>("madrob_benchmark_params");
-          ros::ServiceClient client = rosNode->serviceClient<eurobench_bms_msgs_and_srvs::MadrobBenchmarkParams>("madrob_benchmark_params");
+          ros::ServiceClient client = rosNode.serviceClient<eurobench_bms_msgs_and_srvs::MadrobBenchmarkParams>("madrob_benchmark_params");
         
         eurobench_bms_msgs_and_srvs::MadrobBenchmarkParams srv;
       //  srv.request.a = atoll(argv[1]);
